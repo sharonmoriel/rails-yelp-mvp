a07709d759261959582c60183204545fe708d19e
Sharon Moriel <shmoriel@gmail.com>
2019-11-14 10:41:32 +0000

    frontend add navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '00 44 55 66 77',
+    category:  'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '00 44 44 55 77',
+    category:  'italian'
+  },
+  {
+    name:         'Sushi',
+    address:      'Tokyo',
+    phone_number: '00 33 44 789 77',
+    category:  'japanese'
+  },
+  {
+    name:         'Belgian food',
+    address:      'Belgium',
+    phone_number: '44 44 567 789 32',
+    category:  'belgian'
+  },
+  {
+    name:         'BrieRest',
+    address:      'Paris',
+    phone_number: '33 33 456 789 24',
+    category:  'french'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
